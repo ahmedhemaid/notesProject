e070f32f40ce246f9245b3926176fc732f435e43
Ahmed jaber <aa20589@gmail.com>
2019-12-20 11:07:32 +0200

    edit-note

--- app/src/main/java/com/example/notes/Activities/edit_noteActivity.java
+++ app/src/main/java/com/example/notes/Activities/edit_noteActivity.java
@@ -2,7 +2,11 @@ package com.example.notes.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.notes.R;
 
 public class edit_noteActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class edit_noteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
+    }
+
+    public void showall(View view) {
+        Intent showall=new Intent(this,NotesShowAll.class);
+        startActivity(showall);
+
     }
 }
