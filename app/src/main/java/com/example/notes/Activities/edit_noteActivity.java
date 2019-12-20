31e8a1f4258b003ab00d33ad049fa831e8a5f7fc
Ahmed jaber <aa20589@gmail.com>
2019-12-20 11:07:17 +0200

    edit-note

---
+++ app/src/main/java/com/example/notes/Activities/edit_noteActivity.java
@@ -0,0 +1,14 @@
+package com.example.notes.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_noteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+}
